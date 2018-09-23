14fdc5fa5409e58337635a88c06d6397ec87a505
tuyen-nnt <42695721+tuyen-nnt@users.noreply.github.com>
2018-09-23 17:08:13 +0700

    Update ThutucChaychu.pas

--- ThutucChaychu.pas
+++ ThutucChaychu.pas
@@ -1,26 +1,19 @@
 program Thutuc_chaychu;
 uses crt;
-var s1,s2:string[79];
-function CatDan(s1:string):string;
+var s1,s2:string[79]; i:integer;
+function DinhDang(s1:string):string;
     begin
-       CatDan:=copy(s1,2,length(s1)-1)+s1[1];
-    end;
-procedure CanGiua(var s:string);
-    var i:integer;
-    begin
-      for i:=1 to 40-length(s) div 2 do s:=' '+s;
-    end;
+       DinhDang:=copy(s1,2,length(s1)-1)+s1[1];
+    end; 
 begin
- clrscr;
- write('Nhap xau s1: '); readln(s1);
- CanGiua(s1);
- clrscr;
+ clrscr; write('Nhap xau s1: '); readln(s1);
+ for i:=1 to 40-length(s) div 2 do s1:=' '+s1; clrscr;
  while not (keypressed) do
     begin
       gotoxy(1,12);
       write(s1);
       delay(500);
-      s1:=CatDan(s1);
+      s1:=DinhDang(s1);
     end;
  readln
 end.
