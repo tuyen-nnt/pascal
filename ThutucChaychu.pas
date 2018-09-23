05a47fa66f34e59f073e2651fe130630f42ff28d
tuyen-nnt <42695721+tuyen-nnt@users.noreply.github.com>
2018-09-23 17:33:30 +0700

    Function deleted

--- ThutucChaychu.pas
+++ ThutucChaychu.pas
@@ -1,11 +1,8 @@
 uses crt;
 var s1:string[79]; 
     i:integer;
-function DinhDang(s1:string):string;
-    begin
-       DinhDang:=copy(s1,2,length(s1)-1)+s1[1];
-    end; 
 begin 
+ clrscr;
  write('Nhap xau s1: '); readln(s1); 
  clrscr;
  for i:=1 to 40-length(s) div 2 do s1:=' '+s1;
@@ -14,7 +11,7 @@ begin
       gotoxy(1,12);
       write(s1);
       delay(500);
-      s1:=DinhDang(s1);
+      s1:=copy(s1,2,length(s1)-1)+s1[1];
     end;
  readln
 end.
