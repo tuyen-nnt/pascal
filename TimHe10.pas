25aecebbc89c1197e55ee1841cedb02a23d2116d
tuyen-nnt <contact@tuyen.me>
2018-10-06 00:22:52 +0700

    Chuyen tu He2 sang He10

---
+++ TimHe10.pas
@@ -0,0 +1,22 @@
+uses crt;
+var S:string[8];
+    A:array[1..8] of integer;
+    N,i,k,gt:integer;
+begin
+   clrscr;
+   write('Nhap vao xau nhi phan = ');
+   readln(S);
+   For i:=1 to length(s) do
+     begin
+       if S[i]='1' then A[i]:=1 else A[i]:=0;
+     end;
+   N:=0;
+   for i:=1 to length(S) do
+     begin
+        mu:=1;
+        for k:=1 to length(s)-i do mu:=mu*2;
+        N:= N+A[i]*gt;
+     end;
+   write('So thap phan can tim = ',N);
+   readln
+end.
